38b4d4bd17c1a153a8a7ba144a8b34feeb39d768
athulpm <97athulpm@gmail.com>
2023-02-12 23:56:05 +0530

    Refined sets component

--- src/components/Exercise/Sets.js
+++ src/components/Exercise/Sets.js
@@ -1,22 +1,34 @@
-import { Form } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 
 const Sets = (props) => {
-    return(
-        <Form>
-            <Form.Group>
-                <Form.Label>Set 1</Form.Label>
-                <Form.Control type="number" value={props.sets[1]}/>
-                <Form.Label>Set 2</Form.Label>
-                <Form.Control type="number" value={props.sets[2]}/>
-                <Form.Label>Set 3</Form.Label>
-                <Form.Control type="number" value={props.sets[3]}/>
-                <Form.Label>Set 4</Form.Label>
-                <Form.Control type="number" value={props.sets[4]}/>
-                <Form.Label>Set 5</Form.Label>
-                <Form.Control type="number" value={props.sets[5]}/>
-            </Form.Group>
-        </Form>
-    )
-}
+  return (
+    <Form>
+      <Form.Group
+        style={{
+          display: "flex",
+          alignContent: "space-between",
+          justifyContent: "center",
+        }}
+      >
+        <Row>
+          <Col>
+            <Form.Label>Set 1</Form.Label>
+            <Form.Control type="number" value={props.sets[1]} />
+            <Form.Label>Set 3</Form.Label>
+            <Form.Control type="number" value={props.sets[3]} />
+            <Form.Label>Set 5</Form.Label>
+            <Form.Control type="number" value={props.sets[5]} />
+          </Col>
+          <Col>
+            <Form.Label>Set 2</Form.Label>
+            <Form.Control type="number" value={props.sets[2]} />
+            <Form.Label>Set 4</Form.Label>
+            <Form.Control type="number" value={props.sets[4]} />
+          </Col>
+        </Row>
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default Sets;
