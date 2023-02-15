1aa1e8f8a0fda1efb5879f0f52fd0c6f970cb398
athulpm <97athulpm@gmail.com>
2023-02-15 11:56:29 +0530

    Fixed break timer and improved styling

--- src/components/Exercise/BreakTimer.js
+++ src/components/Exercise/BreakTimer.js
@@ -1,33 +1,63 @@
-import React, { useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 
 const BreakTimer = () => {
-    //states
-    const [timer, setTimer] = useState("03:00");
+  //states
+  const [timer, setTimer] = useState(0);
+  const [timerActive, setTimerActive] = useState(false);
 
-    const countdownTimeInMinutes = 3;
-    const countdownTimeInSeconds = countdownTimeInMinutes * 60;
+  //format time function
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  };
 
-    const formatTime = (timeInSeconds) => {
-        const minutes = Math.floor(timeInSeconds/60);
-        const seconds = timeInSeconds % 60;
-        return `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`
+  //side effects
+  useEffect(() => {
+    let intervalId;
+    if (timerActive && timer < 180) {
+      intervalId = setInterval(() => {
+        setTimer((count) => count + 1);
+      }, 1000);
+    } else if (timer === 180) {
+      setTimerActive(false);
+      setTimer(0);
     }
 
-    //timer
-    const countDownTimer = setInterval(() => {
-        countdownTimeInSeconds--;
-        const updatedTime = formatTime(countdownTimeInSeconds);
-        setTimer(updatedTime);
-        if(countdownTimeInSeconds <= 0) {
-            clearInterval(countDownTimer);
-        }
-    },1000);
+    return () => clearInterval(intervalId);
+  }, [timer, timerActive]);
 
-    return(
-        <React.Fragment>
-            {timer}
-        </React.Fragment>
-    )
-}
+  //handlers
+  const startTimerHandler = () => {
+    setTimerActive(true);
+  };
+
+  return (
+    <React.Fragment>
+      <p>Break Timer:</p>
+      <Row>
+        <Col
+          className="border"
+          style={{ alignItems: "center", justifyContent: "center" }}
+        >
+          {formatTime(timer)}
+        </Col>
+        <Col>
+          <Button
+            variant="primary"
+            onClick={startTimerHandler}
+            style={{ float: "right" }}
+          >
+            Start
+          </Button>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
 
 export default BreakTimer;
