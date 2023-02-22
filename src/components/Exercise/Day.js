0568027089d19b08f60ca415fcc478d834d30b08
athulpm <97athulpm@gmail.com>
2023-02-22 12:04:42 +0530

    minor fixes

--- src/components/Exercise/Day.js
+++ src/components/Exercise/Day.js
@@ -4,10 +4,18 @@ import ExerciseCard from "./ExerciseCard";
 
 const Day = (props) => {
   //store
+  const push = useSelector((state) => state.muscleGroup.push);
+  const legs = useSelector((state) => state.muscleGroup.legs);
   const pull = useSelector((state) => state.muscleGroup.pull);
   let data;
   if (props.day === "pullday") {
     data = pull;
+  }
+  if (props.day === "pushday") {
+    data = push;
+  }
+  if (props.day === "legday") {
+    data = legs;
   }
   return (
     <Container>
