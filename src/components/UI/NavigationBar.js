32ed95e80cd87940d15d552584e6942dc7aec4c0
athulpm <97athulpm@gmail.com>
2023-02-23 17:04:05 +0530

    added dynamic drop down title for nav

--- src/components/UI/NavigationBar.js
+++ src/components/UI/NavigationBar.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const NavigationBar = () => {
+  const [title, setTitle] = useState("Muscle group");
+
+  const dropDownTitleHandler = (day) => {
+    console.log(day);
+    if(day === 'push') {
+      setTitle('Push day');
+    }
+    if(day === 'pull') {
+      setTitle('Pull day');
+    }
+    if(day === 'legs') {
+      setTitle('Leg day');
+    }
+  }
+
   return (
     <Navbar bg="light" style={{border:'1px solid black'}}>
       <Container>
@@ -8,10 +24,10 @@ const NavigationBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav className="me-auto">
-            <NavDropdown title="Muscle group">
-              <NavDropdown.Item href="/push-day">Push day</NavDropdown.Item>
-              <NavDropdown.Item href="/pull-day">Pull day</NavDropdown.Item>
-              <NavDropdown.Item href="/leg-day">Leg day</NavDropdown.Item>
+            <NavDropdown title={title}>
+              <NavDropdown.Item href="/push-day" onClick={dropDownTitleHandler.bind(null, 'push')}>Push day</NavDropdown.Item>
+              <NavDropdown.Item href="/pull-day" onClick={dropDownTitleHandler.bind(null, 'pull')}>Pull day</NavDropdown.Item>
+              <NavDropdown.Item href="/leg-day" onClick={dropDownTitleHandler.bind(null, 'legs')}>Leg day</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
